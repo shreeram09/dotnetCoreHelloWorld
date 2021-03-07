cfd333244d808f881f85171608777d6fb7f9c264
shreeram09 <33367448+shreeram09@users.noreply.github.com>
2021-03-07 08:43:46 +0530

    fix

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-chmod +x /home/ec2-user/aspnetcoreapp/* jar
+chmod +x /home/ec2-user/aspnetcoreapp/* exe
 chmod +x /home/ec2-user/aspnetcoreapp/server_start.sh
 chmod +x /home/ec2-user/aspnetcoreapp/server_stop.sh
